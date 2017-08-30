f320ed63f1131064609b99ff4ad1d603b63c99d0
Rolf Petersen <rolf3d@gmail.com>
2017-08-30 12:33:16 +0200

    Opgave 2

---
+++ BilletSystemDLL/MC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystemDLL
+{
+    public class MC
+    {
+        public string Nummerplade { get; set; }
+        public DateTime dato;
+
+        public MC(string nummerplade)
+        {
+            this.Nummerplade = nummerplade;
+        }
+
+        public int Pris()
+        {
+            return 125;
+        }
+
+        public string Køretøj()
+        {
+            return "MC";
+        }
+
+    }
+}
---
+++ BilletSystemDLLTests/MCTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletSystemDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystemDLL.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void MCTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PrisTest()
+        {
+            // Arrange
+            var mc = new MC("876");
+            // Act
+            int pris = mc.Pris();
+
+            // Assert
+            Assert.AreEqual(125,pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            // Arrange
+            var mc01 = new MC("987");
+
+            // Act
+            string køretøjsslags = mc01.Køretøj();
+
+            // Assert
+            Assert.AreEqual("MC", køretøjsslags);
+        }
+    }
+}
